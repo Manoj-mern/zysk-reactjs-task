dc789efe5db04c3f3e90ca34d14f48b1573dbd82
Manoj-mern <singamanenimanoj@gmail.com>
2024-11-29 11:41:27 +0530

    Initialize project using Create React App

---
+++ nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className={Nav.maincontainer}>
+            <div className={Nav.title}>
+                <h2>Untitled UI</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
